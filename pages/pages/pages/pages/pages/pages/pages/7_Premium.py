76769fa29d9d26079435645a0e14b9bd9e2498a6
Mon20237 <monislove71@gmail.com>
2025-04-20 19:34:55 -0500

    Create 7_Premium.py

---
+++ pages/pages/pages/pages/pages/pages/pages/7_Premium.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("💎 Accede a Calmify+ Premium")
+
+st.markdown("""
+Con Premium obtienes:
+
+- 👩‍⚕️ Contacto directo con psicólogos
+- 📆 Talleres mensuales
+- 📚 Contenido exclusivo
+
+""")
+
+st.button("Probar Premium")
